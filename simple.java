e68c754ba84d8588991a9d2814f53c486f09f6ae
Swara2410 <swaralipawar2410@gmail.com>
2026-01-21 09:51:57 +0530

    Added to demo

---
+++ simple.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Hello World
+        System.out.println("Hello, World!");
+
+        // Addition of two numbers
+        int a = 5;
+        int b = 3;
+        System.out.println("Sum: " + (a + b));
+    }
+}
